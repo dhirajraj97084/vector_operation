07c04ee7a2d928b717ef9c3872606d769722ed94
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-12-03 18:53:54 +0530

    first commit

---
+++ puch_back.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>arr={2,3,4,5};
+    cout<<"size of an array is= "<<arr.size()<<endl;
+    //foreach loop
+    for(int value : arr){
+       cout<<value << " ";
+    }
+    cout<<endl;
+    //push_back operation
+    arr.push_back(230);
+    arr.push_back(40);
+    arr.push_back(0);
+    cout<<"size of an array is= "<<arr.size()<<endl;
+    //foreach loop
+    for(int value : arr){
+       cout<<value << " ";
+    }
+
+}
